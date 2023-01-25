57a627b81e37555abbe48b70dbe5dfff59e4dd3d
zZDANTI <Salvadantizz@gmail.com>
2023-01-25 01:12:52 +0100

    El mejor seguro

--- app/src/main/java/com/example/myapplication/Menu.java
+++ app/src/main/java/com/example/myapplication/Menu.java
@@ -1,6 +1,11 @@
 package com.example.myapplication;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.myapplication.databinding.ActivityMenuBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -29,9 +34,29 @@ public class Menu extends AppCompatActivity {
         NavigationUI.setupWithNavController(binding.navView, navController);
 
 
+
+
+
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.navigation_location:
+                Uri location = Uri.parse("geo:38.970067850258395, 1.2842090553221097?q=1600+Hotel Romeo");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, location);
+                try {
+                    startActivity(mapIntent);
+                } catch (ActivityNotFoundException e) {
+                    Toast.makeText(this, "NO SE PUEDE MOSTRAR EL MAPA", Toast.LENGTH_SHORT).show();
+                }
+                return true;
 
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
 
 
